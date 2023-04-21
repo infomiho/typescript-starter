94eb23d2460d61b4277b8cb9a1079632d9730eed
Fran Zekan <zekan.fran369@gmail.com>
2023-04-21 11:59:52 +0200

    Make it work with vanilla tooling (requires node >19)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import lodashCommon from "lodash";
+import { capitalize as capitalizeESM } from "lodash-es";
+
 console.log("Hello World " + process.env.VARIABLE);
+
+console.log(lodashCommon.capitalize("hello world"));
+console.log(capitalizeESM("hello world"));
